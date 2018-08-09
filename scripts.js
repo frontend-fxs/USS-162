fdc4ffe662805bcab973ed4ec3f7def7d4fc6087
frontend-fxs <oriol@fxstreet.com>
2018-08-09 20:25:20 +0200

    up

--- scripts.js
+++ scripts.js
@@ -1,18 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+(function() {
     var windowWidth = window.innerWidth;
-    window.setTimeout(function() {
-        document.querySelector('#fxs_custom_notification_body').classList.add('fxs_custom_notification_transition');
-        document.querySelector('#fxs_custom_notification_close').addEventListener('click', function() {
-            document.querySelector('#fxs_custom_notification_body').classList.remove('fxs_custom_notification_transition');
+    document.querySelector('#fxs_custom_notification_close').addEventListener('click', function() {
+        document.querySelector('#fxs_custom_notification_body').classList.remove('fxs_custom_notification_animation');
 
-        });
-        document.querySelector('#fxs_custom_notification_link').addEventListener('click', function() {
-            document.querySelector('#fxs_custom_notification_body').classList.remove('fxs_custom_notification_transition');
+    });
+    document.querySelector('#fxs_custom_notification_accept').addEventListener('click', function() {
+        document.querySelector('#fxs_custom_notification_body').classList.remove('fxs_custom_notification_animation');
+    });
+    document.querySelector('#show_custom_notification').addEventListener('click', function() {
+        document.querySelector('#fxs_custom_notification_body').classList.add('fxs_custom_notification_animation');
+    });
 
-        });
-        document.querySelector('#fxs_custom_notification_accept').addEventListener('click', function() {
-            document.querySelector('#fxs_custom_notification_body').classList.remove('fxs_custom_notification_transition');
-
-        });
-    }, 2000);
-});
+})();
